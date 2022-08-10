2fd7e8e95bc690e625a1403550700be7b3347cf6
AIWASS23 <marcelodearaujo677@gmail.com>
2022-08-10 08:20:52 -0300

    Initial Commit

---
+++ SocialRock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialRock
+//
+//  Created by Marcelo De Araújo on 10/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialRock/SocialRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialRockApp.swift
+//  SocialRock
+//
+//  Created by Marcelo De Araújo on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
